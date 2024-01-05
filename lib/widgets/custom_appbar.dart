f8a7862db2bff078c2728b491dae654f32e747e0
v-andrade-dev <vini.andrade0@gmail.com>
2024-01-05 16:58:00 -0300

    [new|prog] Criados widgets de appbar, header, e song_card | #100

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: const Icon(Icons.grid_view_rounded),
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(right: 20),
+          child: CircleAvatar(),
+        )
+      ],
+    );
+  }
+}
