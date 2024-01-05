9e258cbd1e32267fe695a9d0ae5b6b396d04306d
v-andrade-dev <vini.andrade0@gmail.com>
2024-01-05 16:56:51 -0300

    [new|prog] Criada classe model song | #100

---
+++ lib/models/song.dart
@@ -0,0 +1,30 @@
+class Song {
+  final String title;
+  final String description;
+  final String url;
+  final String coverUrl;
+
+  Song(
+      {required this.title,
+      required this.description,
+      required this.url,
+      required this.coverUrl});
+
+  static List<Song> songs = [
+    Song(
+        title: 'Glass',
+        description: 'Glass song',
+        url: 'assets/music/glass.mp3',
+        coverUrl: 'assets/images/glass.jpg'),
+    Song(
+        title: 'Illusions',
+        description: 'Illusions song',
+        url: 'assets/music/illusions.mp3',
+        coverUrl: 'assets/images/illusions.jpg'),
+    Song(
+        title: 'Pray',
+        description: 'Pray song',
+        url: 'assets/music/pray.mp3',
+        coverUrl: 'assets/images/pray.jpg'),
+  ];
+}
